078686908670f047da7f7d0ea3dff2dd11311b96
haxxy09 <htwalibu@gmail.com>
2018-03-26 10:45:19 +0200

    added a tags view

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -84,7 +84,12 @@ exports.getStoreBySlug = async(req, res, next) => {
     const store = await Store.findOne({
         slug: req.params.slug
     });
-
     if(!store) return next();
     res.render('store', {store, title: store.name});
 };
+
+exports.getStoresByTag = async (req, res, next) => {
+    const tags = await Store.getTagsList();
+    const tag = req.params.tag;
+    res.render('tag', { tags, title: 'Tags', tag});
+}
