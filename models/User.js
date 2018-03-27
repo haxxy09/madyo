af56321cec9fb50acc08f1058f3870cf03f2ae57
haxxy09 <htwalibu@gmail.com>
2018-03-27 16:24:38 +0200

    saving registered users to the database

--- models/User.js
+++ models/User.js
@@ -7,22 +7,22 @@ const mongodbErrorHandler = require('mongoose-mongodb-errors');
 const passportLocalMongoose = require('passport-local-mongoose');
 
 const userSchema = new Schema({
-    email: {
-        type: String,
-        unique: true,
-        lowercase: true,
-        trim: true,
-        validate: [validator.isEmail, 'Invalid Email Address'],
-        required: 'Please supply an email address'
-    },
-    name: {
-        type: String,
-        required: 'Please supply a name',
-        trim: true
-    }
+  email: {
+    type: String,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    validate: [validator.isEmail, 'Invalid Email Address'],
+    required: 'Please Supply an email address'
+  },
+  name: {
+    type: String,
+    required: 'Please supply a name',
+    trim: true
+  }
 });
 
-userSchema.plugin(passportLocalMongoose, {usernameField: 'email'});
+userSchema.plugin(passportLocalMongoose, { usernameField: 'email' });
 userSchema.plugin(mongodbErrorHandler);
 
 module.exports = mongoose.model('User', userSchema);
--- routes/index.js
+++ routes/index.js
@@ -2,32 +2,45 @@ const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
 const userController = require('../controllers/userController');
-const {catchErrors} = require ('../handlers/errorHandlers');
+const authController = require('../controllers/authController');
+const { catchErrors } = require('../handlers/errorHandlers');
 
 router.get('/', catchErrors(storeController.getStores));
 router.get('/stores', catchErrors(storeController.getStores));
-router.get('/', storeController.homePage);
 router.get('/add', storeController.addStore);
+
 router.post('/add',
-    storeController.upload,
-    catchErrors(storeController.resize),
-    catchErrors(storeController.createStore));
+  storeController.upload,
+  catchErrors(storeController.resize),
+  catchErrors(storeController.createStore)
+);
+
 router.post('/add/:id',
-    storeController.upload,
-    catchErrors(storeController.resize),
-    catchErrors(storeController.updateStore));
+  storeController.upload,
+  catchErrors(storeController.resize),
+  catchErrors(storeController.updateStore)
+);
 
 router.get('/stores/:id/edit', catchErrors(storeController.editStore));
 router.get('/store/:slug', catchErrors(storeController.getStoreBySlug));
 
-router.get('/tags',catchErrors(storeController.getStoresByTag));
-router.get('/tags/:tag',catchErrors(storeController.getStoresByTag));
+router.get('/tags', catchErrors(storeController.getStoresByTag));
+router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
 
 router.get('/login', userController.loginForm);
 router.get('/register', userController.registerForm);
-router.post('/register', userController.validateRegister);
-// 1. Validate the registration
+
+// 1. Validate the registration data
 // 2. register the user
 // 3. we need to log them in
+router.post('/register',
+  userController.validateRegister,
+  // we need to know about errors if 
+  // validation will be passed, but registration 
+  // will be failed in some reasons, e.g. second 
+  // registration with same email
+  catchErrors(userController.register),
+  authController.login
+);
 
 module.exports = router;
