262861065f375a3e03a9ce39f1261aa871c952ff
haxxy09 <htwalibu@gmail.com>
2018-03-28 17:25:53 +0200

    setup a geospatial endpoint

--- models/Store.js
+++ models/Store.js
@@ -46,6 +46,10 @@ storeSchema.index({
     description: 'text'
 });
 
+storeSchema.index({
+   location:'2dsphere'
+});
+
 storeSchema.pre('save', async function(next) {
   if (!this.isModified('name')) {
     next(); // skip it
@@ -68,6 +72,6 @@ storeSchema.statics.getTagsList = function() {
     { $group: { _id: '$tags', count: { $sum: 1 } } },
     { $sort: { count: -1 } }
   ]);
-}
+};
 
 module.exports = mongoose.model('Store', storeSchema);
